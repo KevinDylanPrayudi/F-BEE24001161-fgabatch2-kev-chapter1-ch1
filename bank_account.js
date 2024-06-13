ec20a432591a780a26247014756daffd4a90b18c
KevinDylanPrayudi <kevin.dylan.prayudi20@gmail.com>
2024-06-13 15:12:17 +0700

    add feat BankAccount class

--- bank_account.js
+++ bank_account.js
@@ -5,3 +5,20 @@ function tambahSaldo(balance, deposit) {
 function kurangiSaldo(balance, withdraw) {
     return balance - withdraw;
 }
+
+class BankAccount {
+    static define(startingBalance) {
+        this.balance = startingBalance
+    }
+    static deposit(amount) {
+        this.balance = tambahSaldo(this.balance, amount)
+        return this.balance
+    }
+    static withdraw(amount) {
+        this.balance = kurangiSaldo(this.balance, amount)
+        return this.balance
+    }
+    static getBalance() {
+        return this.balance
+    }
+}
