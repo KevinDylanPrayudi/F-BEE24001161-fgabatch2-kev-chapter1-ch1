dc0cfab2e37edce575f7982c341e4583ba31e294
KevinDylanPrayudi <kevin.dylan.prayudi20@gmail.com>
2024-06-13 16:11:39 +0700

    Refactor init func & queries func

--- bank_account.js
+++ bank_account.js
@@ -1,6 +1,6 @@
 class BankAccount {
     static define(startingBalance) {
-        return this.balance = startingBalance
+        return this.balance = startingBalance || 0
     }
     static deposit(amount) {
         this.balance = tambahSaldo(this.balance, amount)
@@ -16,36 +16,43 @@ function init(startingBalance, typeOperation) {
     let saldo = startingBalance;
     if (isNaN(saldo) || saldo < 0) {
         alert("Please enter a number or a positive number")
-        return init(+window.prompt("Enter starting balance"), typeOperation)
+        return init(queries("Enter starting balance", 1), typeOperation)
     }
     if (!typeOperation) {
-        typeOperation = window.prompt("Enter another operation: deposit, withdraw, or stop")
+        typeOperation = queries("Enter another operation: start deposit(start), deposit, withdraw, or stop")
+        if (typeOperation === "start") {
+            return init(queries("Enter starting balance", 1), "")
+        }
     }
     saldo = BankAccount.define(saldo)
+    if (!saldo) {
+        return alert(`Your balance is ${saldo}.`)
+    }
     if (typeOperation === "stop" || !typeOperation) { return }
     else if (typeOperation === "deposit") {
-        let deposit = +window.prompt("Enter amount to deposit")
+        let deposit = queries("Enter amount to deposit", 1)
         if (isNaN(deposit) || deposit < 0) {
             alert("Please enter a number or a positive number")
             return init(saldo, typeOperation)
         }
         saldo = BankAccount.deposit(deposit)
     } else if (typeOperation === "withdraw") {
-        let withdraw = +window.prompt("Enter amount to withdraw")
+        let withdraw = queries("Enter amount to withdraw", 1)
         if (isNaN(withdraw) || withdraw < 0 || withdraw > saldo) {
-            alert("Please enter a number, a positive number, or a number lesser than your balance")
+            alert(`Please enter a number, a positive number, or a number lesser than your balance. Your balance is ${saldo}.`)
             return init(saldo, typeOperation)
         }
         saldo = BankAccount.withdraw(withdraw)
     } else {
-        return init(saldo, window.prompt("Enter another operation: deposit, withdraw, or stop"))
+        alert("Please enter 'deposit', 'withdraw', or 'stop'.")
+        return init(saldo, queries("Enter another operation: deposit, withdraw, or stop"))
     }
-    alert("Your new balance is " + saldo)
-    typeOperation = window.prompt("Enter another operation: deposit, withdraw, or stop")
+    alert(`Your new balance is ${saldo}.`)
+    typeOperation = queries("Enter another operation: deposit, withdraw, or stop")
     return init(saldo, typeOperation)
 }
 
-init(, "")
+init(queries("Enter starting balance"), "")
 
 function tambahSaldo(balance, deposit) {
     return balance + deposit;
@@ -55,6 +62,12 @@ function kurangiSaldo(balance, withdraw) {
     return balance - withdraw;
 }
 
-function queries(query) {
-    return +window.prompt(query)
+function queries(query, typeQuery) {
+    let result;
+    if (typeQuery) {
+        result = +window.prompt(query)
+    } else {
+        result = window.prompt(query)
+    }
+    return result
 }
